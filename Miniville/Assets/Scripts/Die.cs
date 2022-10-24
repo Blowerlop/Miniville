9a33f76e5732c099664e34ccd51c287a500cc580
Blowerlop <chrisazur@gmx.fr>
2022-10-24 22:29:48 +0200

    Script Die initialize

--- Miniville/Assets/Scripts/Die.cs
+++ Miniville/Assets/Scripts/Die.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = System.Random;
 
-public class Die : MonoBehaviour
+public abstract class Die : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private int _nbrFaces = 6;
+    private static int _face;
+    private static Random _random = new Random();
 
-    // Update is called once per frame
-    void Update()
+    public static int Roll(int nbrDeDes)
     {
-        
+        _face = 0;
+        int rollResult;
+
+        for (int i = 0; i < nbrDeDes; i++)
+        {
+            rollResult = _random.Next(1, 7);
+            Debug.Log($"Roll result : {rollResult}");
+            _face += rollResult;
+        }
+
+        return _face;
     }
 }
