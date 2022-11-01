8c5ef73a7edd30a2b61431a02b1beb777ce20b5c
Blowerlop <chrisazur@gmx.fr>
2022-11-01 15:12:56 +0100

    Polish

--- Miniville/Assets/Scripts/Popup.cs
+++ Miniville/Assets/Scripts/Popup.cs
@@ -1,16 +1,30 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using TMPro;
 using UnityEngine;
+using DG.Tweening;
+using Unity.VisualScripting;
 
 public class Popup : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI _textMeshProUGUI;
+    private RectTransform _textMeshProUGUIRectTransform;
 
-
-    public void UpdateText(string text)
+    private void Start()
     {
-        _textMeshProUGUI.GetComponent<RectTransform>().transform.position = new Vector2(0, Screen.currentResolution.height / 2);
+        _textMeshProUGUIRectTransform = _textMeshProUGUI.GetComponent<RectTransform>();
+    }
+
+    public async void UpdateText(string text)
+    {
+        _textMeshProUGUIRectTransform.localPosition = new Vector2(0, -Screen.currentResolution.height / 2 + 50);
         _textMeshProUGUI.text = $"{text}'s turn";
+
+        _textMeshProUGUIRectTransform.DOScale(new Vector3(5, 5, 5), 0.0f);
+        await Task.Delay(2000);
+        _textMeshProUGUIRectTransform.DOLocalMoveY(0.0f, 2.0f);
+        _textMeshProUGUIRectTransform.DOScale(Vector3.one, 2.0f);
     }
 }
