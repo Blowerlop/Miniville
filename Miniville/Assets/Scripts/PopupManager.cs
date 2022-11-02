e7271a52b60023c6ce154ae90abe8d6df4e399aa
Kyla Crea <fetuteddypro@gmail.com>
2022-11-02 09:34:21 +0100

    Create PopupManager.cs

---
+++ Miniville/Assets/Scripts/PopupManager.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading;
+
+public class PopupManager : MonoBehaviour
+{
+    public LePopHimself popopup;
+    public List<LePopHimself> SomePopup = new List<LePopHimself>();
+    public string dataa;
+
+    [SerializeField] float horizontalSpeed;
+    [SerializeField] float verticalSpeed;
+
+    public static PopupManager instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Start()
+    {
+        SomePopup = new List<LePopHimself>() { };
+        InvocationPop(dataa);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Eleve()
+    {
+        print(SomePopup.Count);
+    }
+
+    public void InvocationPop(string data)
+    {
+            if (SomePopup.Count > 0)
+            {
+                print("ah ouai ?" + SomePopup.Count);
+                foreach (LePopHimself toi in SomePopup)
+                {
+                    toi.MoveVertical(-75); //Descendre un coup
+                }
+            }
+
+            LePopHimself lui = Instantiate<LePopHimself>(popopup, this.transform.position, Quaternion.identity, this.transform);
+            lui.changetexte(data);
+            SomePopup.Add(lui);
+    }
+}
+ 
